76d7ca8b64d9f062093fa54627ee0394a3c80845
Harsh Panchal <harshhhh1803@gmail.com>
2023-12-08 21:01:54 +0530

    Added Day8.1

---
+++ src/main/kotlin/Day8.kt
@@ -0,0 +1,63 @@
+import java.io.File
+
+/*
+--- Day 8: Haunted Wasteland ---
+You're still riding a camel across Desert Island when you spot a sandstorm quickly approaching. When you turn to warn the Elf, she disappears before your eyes! To be fair, she had just finished warning you about ghosts a few minutes ago.
+One of the camel's pouches is labeled "maps" - sure enough, it's full of documents (your puzzle input) about how to navigate the desert. At least, you're pretty sure that's what they are; one of the documents contains a list of left/right instructions, and the rest of the documents seem to describe some kind of network of labeled nodes.
+It seems like you're meant to use the left/right instructions to navigate the network. Perhaps if you have the camel follow the same instructions, you can escape the haunted wasteland!
+After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You feel like AAA is where you are now, and you have to follow the left/right instructions until you reach ZZZ.
+This format defines each node of the network individually. For example:
+
+Explain
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+
+Starting with AAA, you need to look up the next element based on the next left/right instruction in your input.
+In this example, start with AAA and go right (R) by choosing the right element of AAA, CCC.
+Then, L means to choose the left element of CCC, ZZZ. By following the left/right instructions, you reach ZZZ in 2 steps.
+
+Of course, you might not find ZZZ right away. If you run out of left/right instructions, repeat the whole sequence of instructions as necessary: RL really means RLRLRLRLRLRLRLRL... and so on. For example, here is a situation that takes 6 steps to reach ZZZ:
+
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+
+Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
+*/
+
+class Day8(input: List<String>) {
+
+    private val dirs = input.first().map { if (it == 'L') 0 else 1 }
+
+    private val nodes =
+        input.drop(2).associate { it.substringBefore(" =") to it.substringAfter("(").substringBefore(")").split(", ") }
+
+    private fun steps(node: String, repeat: (node: String) -> Boolean): Long {
+        var steps = 0L
+        var current = node
+
+        while (repeat(current)) {
+            current = nodes[current]!![dirs[(steps++ % dirs.size).toInt()]]
+        }
+
+        return steps
+    }
+
+    fun solution1(): Long {
+        return steps(node = "AAA", repeat = { it != "ZZZ" })
+    }
+}
+
+fun main() {
+    val fileInput: List<String> = File("src/main/kotlin/inputs/Day8.txt").readLines()
+    println(Day8(fileInput).solution1())
+}
