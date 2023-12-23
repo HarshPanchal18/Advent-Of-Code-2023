fd92730ddd66b2c7200607d006cac4b5ada09e9f
Harsh Panchal <harshhhh1803@gmail.com>
2023-12-23 11:32:12 +0530

    Added Day21.1

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -102,6 +102,24 @@ data class Point(val x: Int, val y: Int) {
     operator fun plus(other: Point) = copy(x = x + other.x, y = y + other.y)
     operator fun plus(other: Direction) = plus(other.move)
     fun distanceTo(other: Point) = (x - other.x).absoluteValue + (y - other.y).absoluteValue
+
+    fun orthogonalNeighbours() = setOf(
+        copy(x = x - 1),
+        copy(x = x + 1),
+        copy(y = y - 1),
+        copy(y = y + 1),
+    )
+
+    fun neighbours() = setOf(
+        copy(y = y - 1),
+        copy(x = x + 1, y = y - 1),
+        copy(x = x + 1),
+        copy(x = x + 1, y = y + 1),
+        copy(y = y + 1),
+        copy(x = x - 1, y = y + 1),
+        copy(x = x - 1),
+        copy(x = x - 1, y = y - 1),
+    )
 }
 
 private typealias DirectedPoint = Pair<Point, Direction>
